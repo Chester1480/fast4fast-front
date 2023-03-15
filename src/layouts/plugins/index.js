985c1c2a3802c389883e8b3ea595588b65b4b5ea
chester <chester0148@gmail.com>
2023-03-16 01:36:01 +0800

    加入多國語系 vue-i18n

--- src/layouts/plugins/index.js
+++ src/layouts/plugins/index.js
@@ -9,9 +9,25 @@ import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
 import router from '@/router'
 
+import { createI18n, useI18n } from 'vue-i18n'
+import en from '../../locales/en.json'
+import zhHant from '../../locales/zh-Hant.json'
+
+const messages = {
+  "en":en ,
+  "zh-Hant":zhHant
+}
+
+const i18n = new createI18n({
+  legacy: false, // Vuetify does not support the legacy mode of vue-i18n
+  locale: 'zh-Hant',
+  fallbackLocale: 'en',
+  messages,
+})
+
 export function registerPlugins (app) {
   loadFonts()
-  app
+  app.use(i18n)
     .use(vuetify)
     .use(router)
 }
