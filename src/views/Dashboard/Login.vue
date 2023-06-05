eff7ac331e99f0d432ec2929e39f7ad1dbcab29e
chester <chester0148@gmail.com>
2023-06-06 02:14:51 +0800

    add: 新增 ws 套件

--- src/views/Dashboard/Login.vue
+++ src/views/Dashboard/Login.vue
@@ -3,20 +3,19 @@
         <v-container fluid>
             <v-sheet width="500" class="mx-auto">
                 <v-form fast-fail @submit.prevent>
+                  
                     <v-text-field
                     v-model="account"
                     label="account"
-                    :rules="accountRules"
                     ></v-text-field>
             
                     <v-text-field
                     v-model="password"
                     label="password"
                     type="password"
-                    :rules="passwordRules"
                     ></v-text-field>
-            
-                    <v-btn type="submit" @click="post" block class="mt-2">Submit</v-btn>
+                    <!-- <button v-on:click="login" block class="mt-2">Login</button> -->
+                    <v-btn  @click="postApi" block class="mt-2">Login</v-btn>
                 </v-form>
             </v-sheet>
         </v-container>
@@ -25,7 +24,7 @@
   
   <script>
     import { fetchPost } from '../../utils/url';
-    // import { getApiUrl } from '../../utils/url';
+    import { getDataByType } from '../../utils/parameters';
 
     const url = window.location.origin;
     
@@ -35,29 +34,27 @@
         accountRules: [
           value => {
             if (value?.length > 6) return true
-            return 'First name must be at least 6 characters.'
+            // return 'First name must be at least 6 characters.'
           },
         ],
         password: '',
         passwordRules: [
           value => {
             if (/[^0-9]/.test(value)) return true
-            return 'Last name can not contain digits.'
+            // return 'Last name can not contain digits.'
           },
         ],
       }),
       async method() {
-          async function post(){
-            const data = {
+          async function postApi(){
+            const data = getDataByType();
+            console.log(data);
+            // const result = await fetchPost(url, data);
+            // if (result) {
               
-            }
-
-            const result = await fetchPost(url, data);
-            if (result) {
+            // } else {
               
-            } else {
-              
-            }
+            // }
             
           }
       },
