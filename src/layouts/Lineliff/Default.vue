0df6a31627fffa153351fdc16d7b63523dbe6273
chester <chester0148@gmail.com>
2023-08-08 20:23:02 +0800

    refactor: 調整新版UI

--- src/layouts/Lineliff/Default.vue
+++ src/layouts/Lineliff/Default.vue
@@ -1,55 +1,53 @@
 <template>
    <v-app>
-
-        <v-main>
-            <v-window v-model="tab">
-                  <v-window-item
-                    v-for="i in 3"
-                    :key="i"
-                    :value="'tab-' + i"
-                  >
-                    <v-card>
-                      <v-card-text>
-                        <router-view />
-                      </v-card-text>
-                    </v-card>
-                  </v-window-item>
-              </v-window>
-        </v-main>
-
-        <menu>
-              
-            <v-card>
-             
-              <v-tabs
-                v-model="tab"
-                :bg-color="tabs.backgroundColor"
-                align-tabs="center"
-                class="d-flex justify-center"
-                fixed-tabs 
-              >
-                
-                <v-tab  value="tab-1" to="/Lineliff" >
-                  <v-icon>mdi-home</v-icon>
-                </v-tab>
-
-                <v-tab value="tab-2" to="/Lineliff/Search"   >
-                  <v-icon>mdi-magnify</v-icon>
-                </v-tab>
-                
-                <v-tab value="tab-3" >
-                  <v-icon>mdi-home</v-icon>
-                </v-tab>
-
-                <v-tab value="tab-4" >
-                  <v-icon>mdi-account</v-icon>
-                </v-tab>
-                
-              </v-tabs>
-             
+        <v-main class="overflow-hidden" style="overflow: scroll; border: 1px solid red;">
+          <v-container class="fill-height" fluid>
+            <v-card style="width: 100%;">
+                <v-card-text>
+                  <v-window v-model="tab">
+                      <v-window-item
+                        v-for="i in 3"
+                        :key="i"
+                        :value="'tab-' + i"
+                      >
+                        <v-card>
+                          <v-card-text>
+                            <router-view />
+                          </v-card-text>
+                        </v-card>
+                      </v-window-item>
+                  </v-window>
+              </v-card-text>
             </v-card>
-        </menu>
+          </v-container>
+        </v-main>
+        <v-footer color="indigo" app>
+          <v-tabs
+              v-model="tab"
+              :bg-color="tabs.backgroundColor"
+              align-tabs="center"
+              class="d-flex justify-center"
+              fixed-tabs 
+            >
+              
+              <v-tab  value="tab-1" to="/Lineliff" >
+                <v-icon>mdi-home</v-icon>
+              </v-tab>
 
+              <v-tab value="tab-2" to="/Lineliff/Search"   >
+                <v-icon>mdi-magnify</v-icon>
+              </v-tab>
+              
+              <v-tab value="tab-3" >
+                <v-icon>mdi-home</v-icon>
+              </v-tab>
+
+              <v-tab value="tab-4" >
+                <v-icon>mdi-account</v-icon>
+              </v-tab>
+              
+            </v-tabs>
+        </v-footer>
     </v-app>
   </template>
   
@@ -91,14 +89,14 @@
 </script>
 
 <style>
-  menu {
-    color: #fff;
-    padding: 10px;
+  /* menu {
     text-align: center;
     vertical-align: bottom;
-  }
+  }  */
 
-  main {
+  /* main {
     padding: 100px;
-  }
+    text-align: center;
+    vertical-align: top;
+  } */
 </style>
